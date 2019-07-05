2c1a40903bf17b643f26e81cc9416c72f2f60e5e
AndranikMikhailyan <andranik.stav@mail.ru>
2019-07-06 00:17:16 +0300

    Lesson 88.

--- city-register/src/test/java/edu/javacourse/city/dao/PersonCheckDaoTest.java
+++ city-register/src/test/java/edu/javacourse/city/dao/PersonCheckDaoTest.java
@@ -7,9 +7,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import static org.junit.Assert.*;
 
 public class PersonCheckDaoTest {
 
