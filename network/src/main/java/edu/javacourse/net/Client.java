0e406f187d87fb211525ed57d7a500a451256e56
AndranikMikhailyan <andranik.stav@mail.ru>
2019-07-06 23:42:04 +0300

    Lesson 90. Multithread

--- network/src/main/java/edu/javacourse/net/Client.java
+++ network/src/main/java/edu/javacourse/net/Client.java
@@ -2,30 +2,43 @@ package edu.javacourse.net;
 
 import java.io.*;
 import java.net.Socket;
+import java.time.LocalDateTime;
 
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        sendRequest();
+        for (int i = 0; i < 1000; i++) {
+            SimpleClient sc = new SimpleClient();
+            sc.start();
+        }
     }
+}
 
-    private static void sendRequest() throws IOException {
-        Socket socket = new Socket("localhost", 25225);
+class SimpleClient extends Thread {
+    @Override
+    public void run() {
+        try {
+            System.out.println("Started: " + LocalDateTime.now());
+            Socket socket = new Socket("localhost", 25225);
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-        String sb = "Noname";
-        bw.write(sb);
-        bw.newLine();
-        bw.flush();
+            String sb = "Noname";
+            bw.write(sb);
+            bw.newLine();
+            bw.flush();
 
-        String answer = br.readLine();
-        System.out.println(answer);
+            String answer = br.readLine();
+            System.out.println(answer);
 
-        br.close();
-        bw.close();
+            br.close();
+            bw.close();
 
-        socket.close();
+            socket.close();
+            System.out.println("Finished: " + LocalDateTime.now());
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
     }
 }
--- network/src/main/java/edu/javacourse/net/Server.java
+++ network/src/main/java/edu/javacourse/net/Server.java
@@ -6,30 +6,52 @@ import java.net.Socket;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket socket = new ServerSocket(25225);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket socket = new ServerSocket(25225, 2000);
 
         while (true) {
             Socket client = socket.accept();
-            handlerRequest(client);
+            new SimpleServer(client).start();
         }
     }
 
-    private static void handlerRequest(Socket client) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+}
 
-        StringBuilder sb = new StringBuilder("Hello, ");
-        String userName = br.readLine();
+class SimpleServer extends Thread {
+    private Socket client;
 
-        sb.append(userName);
-        bw.write(sb.toString());
-        bw.newLine();
-        bw.flush();
-
-        br.close();
-        bw.close();
-
-        client.close();
+    SimpleServer(Socket client) {
+        this.client = client;
     }
+
+    @Override
+    public void run() {
+        handlerRequest(client);
+    }
+
+    private void handlerRequest(Socket client) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+            StringBuilder sb = new StringBuilder("Hello, ");
+            String userName = br.readLine();
+            System.out.println("Server got string: " + userName);
+            Thread.sleep(2000);
+
+            sb.append(userName);
+            bw.write(sb.toString());
+            bw.newLine();
+            bw.flush();
+
+            br.close();
+            bw.close();
+
+            client.close();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+
 }
