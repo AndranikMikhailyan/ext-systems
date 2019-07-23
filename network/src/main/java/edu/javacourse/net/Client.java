d570fad5cb0f95910ad95d9ff17e8fe4b6e77ca8
AndranikMikhailyan <andranik.stav@mail.ru>
2019-07-23 20:10:41 +0300

    Урок 91. Реакция сервера на разные команды

--- network/src/main/java/edu/javacourse/net/Client.java
+++ network/src/main/java/edu/javacourse/net/Client.java
@@ -7,24 +7,36 @@ import java.time.LocalDateTime;
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 1000; i++) {
-            SimpleClient sc = new SimpleClient();
+        for (int i = 0; i < 8; i++) {
+            SimpleClient sc = new SimpleClient(i);
             sc.start();
         }
     }
 }
 
 class SimpleClient extends Thread {
+
+    private final static String[] COMMAND = {
+            "HELLO", "MORNING", "DAY", "EVENING"
+    };
+
+    private int cmdNumber;
+
+    public SimpleClient(int cmdNumber) {
+        this.cmdNumber = cmdNumber;
+    }
+
     @Override
     public void run() {
         try {
-            System.out.println("Started: " + LocalDateTime.now());
+//            System.out.println("Started: " + LocalDateTime.now());
             Socket socket = new Socket("localhost", 25225);
 
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            String sb = "Noname";
+            String command = COMMAND[cmdNumber % COMMAND.length];
+            String sb = command + " " + "Noname";
             bw.write(sb);
             bw.newLine();
             bw.flush();
@@ -36,7 +48,7 @@ class SimpleClient extends Thread {
             bw.close();
 
             socket.close();
-            System.out.println("Finished: " + LocalDateTime.now());
+//            System.out.println("Finished: " + LocalDateTime.now());
         } catch (IOException ex) {
             ex.printStackTrace(System.out);
         }
